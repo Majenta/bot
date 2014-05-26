b96f736f6400bb781afd185096ea20f78db04e1d
Majenta <master@majenta.tk>
2014-05-27 00:10:17 +0900

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('core/index.php');
-$bot = new bot();
-//echo $bot -> get('vk.com') -> body;
-$vk = $bot -> loadProvider('vk');
-echo $vk -> login('prostocyber@gmail.com', '321rjynfrn67');
