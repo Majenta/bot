2f4ccfbbd8f81a3783add4dc7aa35c276fea3358
Majenta <master@majenta.tk>
2014-05-27 00:05:50 +0900

    Added redirection recursive parameter

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 	define('ROOT', __DIR__); // Путь к корневой директории скрипта
 	define('FOLLOW_LOCATION', true); // Разрешить редирект
+	define('FOLLOW_LOCATION_DEPTH', 3); // Глубина рекурсии при редиректе
 ?>
